5595ac5969113dc21ca09fcfb2c2302a337cc770
badAl <badalsingh120403@gmail.com>
2025-10-31 14:27:07 +0530

    refactor: Refactor HTTP logger to use performance.now()

--- apps/analytics-service/src/middlewares/http-logger.middleware.ts
+++ apps/analytics-service/src/middlewares/http-logger.middleware.ts
@@ -1,20 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import { performance } from 'perf_hooks';
+
 import logger from '../config/logger';
 
 export const httpLogger = (req: Request, res: Response, next: NextFunction) => {
-  const startTime = Date.now();
+  const startTime = performance.now();
 
   res.on('finish', () => {
-    const duration = Date.now() - startTime;
+    const duration = performance.now() - startTime;
 
-    if (
-      req.originalUrl.startsWith('/api/auth/health') &&
-      res.statusCode === 200
-    )
+    if (req.originalUrl.startsWith('/api/v1/health') && res.statusCode === 200) {
       return;
+    }
 
     logger.info(
-      `HTTP Request: ${req.method} ${req.originalUrl} - Status ${res.statusCode} - ${duration}ms`
+      `HTTP Request: ${req.method} ${req.originalUrl} - Status ${res.statusCode} - ${duration.toFixed(2)}ms`
     );
   });
 
