0adf06fdad03186103f7d707f55db0d8b156cccc
Badal <badalsingh120403@gmail.com>
2025-11-07 06:44:10 +0530

    chore: expo lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import prettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -8,16 +10,16 @@ import tseslint from 'typescript-eslint';
  */
 export default tseslint.config(
   ...tseslint.configs.recommended,
+
   {
+    files: ['apps/analytics-service/**/*.ts'],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.vitest,
       },
     },
-  },
 
-  {
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'warn',
@@ -37,9 +39,30 @@ export default tseslint.config(
   },
 
   {
+    files: ['apps/mobile-app/**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      ...reactPlugin.configs.recommended,
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
+  {
     ignores: [
       'node_modules/',
       'dist/',
+      'apps/*/dist',
       'coverage/',
       '.pnpm-store/',
       'pnpm-lock.yaml',
